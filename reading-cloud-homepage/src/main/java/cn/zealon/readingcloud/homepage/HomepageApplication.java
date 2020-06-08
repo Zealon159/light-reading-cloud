0ad33b029f64a90420de5e036ad3f762495c88e9
Zealon <zealon@126.com>
2020-06-08 16:09:44 +0800

    :four_leaf_clover: 去掉Feign调试日志.

--- reading-cloud-homepage/src/main/java/cn/zealon/readingcloud/homepage/HomepageApplication.java
+++ reading-cloud-homepage/src/main/java/cn/zealon/readingcloud/homepage/HomepageApplication.java
@@ -1,16 +1,12 @@
 package cn.zealon.readingcloud.homepage;
 
-import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @EnableFeignClients(basePackages = {"cn.zealon.readingcloud.book.feign", "cn.zealon.readingcloud.account.feign"})
-//@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = {"cn.zealon.readingcloud.homepage", "cn.zealon.readingcloud.common", "cn.zealon.readingcloud.book.feign", "cn.zealon.readingcloud.account.feign"})
 @EnableHystrix
 @EnableHystrixDashboard
@@ -19,11 +15,5 @@ public class HomepageApplication {
     public static void main(String[] args) {
         SpringApplication.run(HomepageApplication.class, args);
     }
-
-    @Bean
-    public Logger.Level feignLoggerLevel(){
-        return  Logger.Level.FULL;
-    }
-
 
 }
